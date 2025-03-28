75a2b633fda3b81d495ea4be2e6449aec540c0e4
Ahmed Mostafa <ahmed.mostafa1198@gmail.com>
2025-03-28 03:30:44 +0200

    Enhancement: Refactor Saving State Management in Image Editor
    - Updated `setSaving` action to utilize a structured `loadingData` object
    - Added support for custom loading text and cancel function in saving state

--- packages/react-filerobot-image-editor/src/actions/setSaving.js
+++ packages/react-filerobot-image-editor/src/actions/setSaving.js
@@ -3,7 +3,11 @@ export const SET_SAVING = 'SET_SAVING';
 const setSaving = (state, payload) => ({
   ...state,
   isSaving: payload.isSaving,
-  isLoadingGlobally: payload.isSaving,
+  loadingData: {
+    isLoadingGlobally: payload.isSaving,
+    text: payload.loadingText || '',
+    cancelFn: payload.loadingCancelFn || null,
+  },
 });
 
 export default setSaving;
